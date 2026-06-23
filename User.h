b44bf9968eba6a2e5526a729da5568bf8035194c
marsz404 <122535742+marsz404@users.noreply.github.com>
2026-06-23 16:49:27 +0700

    Update User.h

--- User.h
+++ User.h
@@ -1,1 +1,23 @@
+#pragma once
+#include <string>
 
+using namespace std;
+
+class User
+{
+protected:
+	string id;
+	string name;
+	string role;
+public:
+	User(std::string uId, std::string uName, std::string uRole);
+	virtual ~User() {}
+
+	string getID() const;
+	string getName() const;
+	string getRole() const;
+
+	void setName(string n);
+
+	virtual void displayMenu() = 0;
+};
