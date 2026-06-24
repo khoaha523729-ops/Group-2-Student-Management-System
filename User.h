c0f08fa64aee0693f96d2058aa4561a6634b64f8
marsz404 <122535742+marsz404@users.noreply.github.com>
2026-06-24 18:35:24 +0700

    Duy: update User.h, bỏ role
    Removed role attribute and updated constructor accordingly.

--- User.h
+++ User.h
@@ -9,18 +9,16 @@ protected:
     string id;
     string pass;
     string name;
-    string role;
     string birthday;
     string gender;
 
 public:
-    User(string uId, string uPass, string uName, string uRole,  string uGender, string uBirthday);
+    User(string uId, string uPass, string uName, string uGender, string uBirthday);
 
     virtual ~User() = default;
 
     string getID() const;
     string getName() const;
-    string getRole() const;
     string getBirthday() const;
     string getGender() const;
 
