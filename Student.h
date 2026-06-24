1c4107f2850f3869a58c322d7ecf3dbc1165b4b1
marsz404 <122535742+marsz404@users.noreply.github.com>
2026-06-24 18:54:57 +0700

    Duy: update Student.h

--- Student.h
+++ Student.h
@@ -1,34 +1,17 @@
 #pragma once
-#include <vector>
 #include "User.h"
 #include <string>
 
 using namespace std;
 
-class Course;
-
 class Student : public User
 {
 private:
-	string major;
-	vector<Course*> enrolledCourses;
-	string field;
+    string major;
+
 public:
-	Student(string uId, string name, string major);
-	bool enrollCourse(Course* c);
-	bool leaveCourse(Course* c);
-	void showCourse() const;
-	void showGrade() const;
-	string getMajor() const;
+    Student(string id, string pass, string name, string gender, string birthday, string major);
+    string getMajor() const;
 
-	void display() override;
-
-
-	// import file student.txt
-	Student(string sId, string sPass, string sName, 
-            string sGender, string sBirthday, string sField)
-        : User(sId, sPass, sName, sGender, sBirthday), field(sField){}
-
-	
-	// thieu constructor hien tat ca course hien co
+    void displayMenu() override;
 };
