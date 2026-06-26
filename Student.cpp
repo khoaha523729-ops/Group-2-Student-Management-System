f12c7a68bb29a7d9b7aa8280e49991a1c495c497
marsz404 <122535742+marsz404@users.noreply.github.com>
2026-06-26 12:31:16 +0700

    Duy: update và xóa menu trong constructor
    Removed dependency on Database class and adjusted constructor.

--- Student.cpp
+++ Student.cpp
@@ -1,19 +1,9 @@
 #include "Student.h"
-#include "Database.h"
 #include <iostream>
 
-Student::Student(string id, string pass, string name, string gender, string birthday, string major, Database& db)
+Student::Student(string id, string pass, string name, string gender, string birthday, string major)
     : User(id, pass, name, gender, birthday), major(major)
 {
-    cout << "\n===== ALL COURSES =====\n";
-
-    for (int i = 0; i < db.courseList.size(); i++)
-    {
-        cout << db.courseList[i].getCourseID() << " "
-            << db.courseList[i].getCourseName() << endl;
-    }
-
-    cout << endl;
 }
 
 string Student::getMajor() const
